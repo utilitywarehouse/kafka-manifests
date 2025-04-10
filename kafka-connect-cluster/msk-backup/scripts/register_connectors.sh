f9b09d9d87d9649db7e5dfa257c0b39b1e47ecc0
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2025-04-10 11:09:00 +0300

    Update to store as yaml

--- kafka-connect-cluster/msk-backup/scripts/register_connectors.sh
+++ kafka-connect-cluster/msk-backup/scripts/register_connectors.sh
@@ -16,34 +16,35 @@ done
 
 echo "Kafka Connect REST API is available. Starting connector registration process."
 
-for CONFIG_FILE in "$CONNECTOR_CONFIG_DIR"/*.json; do
-  if [ ! -f "$CONFIG_FILE" ]; then
-    echo "No JSON files found in $CONNECTOR_CONFIG_DIR. Skipping."
+for payload_file in "$CONNECTOR_CONFIG_DIR"/*.yaml; do
+  if [ ! -f "$payload_file" ]; then
+    echo "No YAML files found in $CONNECTOR_CONFIG_DIR. Skipping."
     break
   fi
 
-  CONNECTOR_NAME=$(jq -r '.name' "$CONFIG_FILE")
-  if [ -z "$CONNECTOR_NAME" ] || [ "$CONNECTOR_NAME" == "null" ]; then
-    echo "Skipping $CONFIG_FILE: Missing or invalid connector payload."
+  connector_name=$(yq -r '.name' "$payload_file")
+  if [ -z "$connector_name" ] || [ "$connector_name" == "null" ]; then
+    echo "Skipping $payload_file: Missing or invalid connector payload."
     continue
   fi
 
-  echo "Registering connector: $CONNECTOR_NAME using payload file: $CONFIG_FILE"
+  echo "Registering connector: $connector_name using payload file: $payload_file"
 
-  RESPONSE=$(curl -s -w "\n%{http_code}" \
+  # convert to json from yaml before sending to the kafka connect API as that's the only accepted format
+  resp=$(yq -o=json "${payload_file}" | curl -s -w "\n%{http_code}" \
     -X POST -H "Content-Type: application/json" \
-    --data @"$CONFIG_FILE" "$CONNECTOR_REGISTRATION_ENDPOINT")
+    --data @- "$CONNECTOR_REGISTRATION_ENDPOINT")
 
-  RESPONSE_BODY=$(echo "$RESPONSE" | sed '$d')
-  RESPONSE_CODE=$(echo "$RESPONSE" | tail -n 1)
+  rbody=$(echo "$resp" | sed '$d')
+  rcode=$(echo "$resp" | tail -n 1)
 
-  if [ "$RESPONSE_CODE" -eq 409 ]; then
-    echo "Connector '$CONNECTOR_NAME' already registered. Skipping."
-  elif [ "$RESPONSE_CODE" -ge 200 ] && [ "$RESPONSE_CODE" -lt 300 ]; then
-    echo "Connector '$CONNECTOR_NAME' registered successfully!"
+  if [ "$rcode" -eq 409 ]; then
+    echo "Connector '$connector_name' already registered. Skipping."
+  elif [ "$rcode" -ge 200 ] && [ "$rcode" -lt 300 ]; then
+    echo "Connector '$connector_name' registered successfully!"
   else
-    echo "Failed to register connector '$CONNECTOR_NAME'. HTTP status code: $RESPONSE_CODE"
-    echo "$RESPONSE_BODY"
+    echo "Failed to register connector '$connector_name'. HTTP status code: $rcode"
+    echo "$rbody"
   fi
 done
 
