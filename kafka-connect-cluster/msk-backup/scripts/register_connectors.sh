d96dbfd1c6e942d531ee989bbd8557781a8f52b5
sbuliarca <8848332+sbuliarca@users.noreply.github.com>
2025-04-10 16:56:19 +0300

    DENA 1341 payload as yaml (#82)
    * Update to store as yaml
    
    * Set the job to be deleted before creation
    
    * Updated script to handle updates of the config.
    
    * Fixed the connector config
    
    * Test an update of the config through Argo
    
    * Include post sync policy for the hook
    
    * Revert "Test an update of the config through Argo"
    
    This reverts commit 745f6ea0ba164b547cb5375fdb85e556c3fa989d.
    
    * Added more documentation to the connector file

--- kafka-connect-cluster/msk-backup/scripts/register_connectors.sh
+++ kafka-connect-cluster/msk-backup/scripts/register_connectors.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-CONNECTOR_REGISTRATION_ENDPOINT="${KAFKA_CONNECT_API_HOST:-http://localhost:8083}/connectors"
+CONNECTORS_ENDPOINT="${KAFKA_CONNECT_API_HOST:-http://localhost:8083}/connectors"
 CONNECTOR_CONFIG_DIR="${CONNECTOR_CONFIG_DIR:-/config/connectors}"
 
 if [ ! -d "$CONNECTOR_CONFIG_DIR" ]; then
@@ -9,41 +9,43 @@ if [ ! -d "$CONNECTOR_CONFIG_DIR" ]; then
 fi
 
 echo "Waiting for Kafka Connect REST API..."
-while ! curl -s "$CONNECTOR_REGISTRATION_ENDPOINT" > /dev/null; do
+while ! curl -s "$CONNECTORS_ENDPOINT" > /dev/null; do
   echo "Kafka Connect REST API is not available yet. Retrying in 5 seconds..."
   sleep 5
 done
 
 echo "Kafka Connect REST API is available. Starting connector registration process."
 
-for CONFIG_FILE in "$CONNECTOR_CONFIG_DIR"/*.json; do
-  if [ ! -f "$CONFIG_FILE" ]; then
-    echo "No JSON files found in $CONNECTOR_CONFIG_DIR. Skipping."
+for payload_file in "$CONNECTOR_CONFIG_DIR"/*.yaml; do
+  if [ ! -f "$payload_file" ]; then
+    echo "No YAML files found in $CONNECTOR_CONFIG_DIR. Skipping."
     break
   fi
 
-  CONNECTOR_NAME=$(jq -r '.name' "$CONFIG_FILE")
-  if [ -z "$CONNECTOR_NAME" ] || [ "$CONNECTOR_NAME" == "null" ]; then
-    echo "Skipping $CONFIG_FILE: Missing or invalid connector payload."
+  connector_name=$(yq -r '.name' "$payload_file")
+  if [ -z "$connector_name" ] || [ "$connector_name" == "null" ]; then
+    echo "Skipping $payload_file: Missing or invalid connector payload."
     continue
   fi
 
-  echo "Registering connector: $CONNECTOR_NAME using payload file: $CONFIG_FILE"
+  echo "Registering connector: $connector_name using payload file: $payload_file"
 
-  RESPONSE=$(curl -s -w "\n%{http_code}" \
-    -X POST -H "Content-Type: application/json" \
-    --data @"$CONFIG_FILE" "$CONNECTOR_REGISTRATION_ENDPOINT")
+  config_endpoint="${CONNECTORS_ENDPOINT}/${connector_name}/config"
 
-  RESPONSE_BODY=$(echo "$RESPONSE" | sed '$d')
-  RESPONSE_CODE=$(echo "$RESPONSE" | tail -n 1)
+  # convert to json from yaml before sending to the kafka connect API as that's the only accepted format
+  resp=$(yq -r '.config' -o=json "${payload_file}" | curl -s -w "\n%{http_code}" \
+    -X PUT -H "Content-Type: application/json" \
+    --data @- "${config_endpoint}")
 
-  if [ "$RESPONSE_CODE" -eq 409 ]; then
-    echo "Connector '$CONNECTOR_NAME' already registered. Skipping."
-  elif [ "$RESPONSE_CODE" -ge 200 ] && [ "$RESPONSE_CODE" -lt 300 ]; then
-    echo "Connector '$CONNECTOR_NAME' registered successfully!"
+  rbody=$(echo "$resp" | sed '$d')
+  rcode=$(echo "$resp" | tail -n 1)
+
+  if [ "$rcode" -ge 200 ] && [ "$rcode" -lt 300 ]; then
+    echo "Connector '$connector_name' registered successfully!"
   else
-    echo "Failed to register connector '$CONNECTOR_NAME'. HTTP status code: $RESPONSE_CODE"
-    echo "$RESPONSE_BODY"
+    echo "Failed to register connector '$connector_name'. HTTP status code: $rcode"
+    echo "$rbody"
+    exit 1
   fi
 done
 
